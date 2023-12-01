ead34fb9d7e1f414ef9e68d1cce96ccc4da99c7d
sayandip-ghosh <143954657+sayandip-ghosh@users.noreply.github.com>
2023-12-02 03:01:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+Shery.imageMasker(".featurette-image", {
+
+    mouseFollower: true,
+    text: "SAYANDIP",
+    ease: "cubic-bezier(0.23, 1, 0.320, 2)",
+    duration: 1,
+});
+
+Shery.textAnimate(".sher", {
+
+    style: 1,
+    y: 10,
+    delay: 0.21,
+    duration: 1,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0.2,
+});
+
+Shery.hoverWithMediaCircle(".hov", {
+    images: [],
+});
